40992446fd6d6abe79eec9a7c9527e396ef2d8b6
Reza Tayebi <reza.tayebi73@gmail.com>
2025-12-29 23:15:20 +0100

    Refactor Command enum to include TTL and Expire commands, and update parse method to handle new command structure and TTL parsing.

--- src/command.rs
+++ src/command.rs
@@ -1,35 +1,77 @@
 pub enum Command {
-    Get { key: String },
-    Set { key: String, value: String },
-    Delete { key: String },
-    Rename { old_key: String, new_key: String },
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        value: String,
+        ttl: u64,
+    },
+    Delete {
+        key: String,
+    },
+    Rename {
+        old_key: String,
+        new_key: String,
+    },
+    Expire {
+        key: String,
+        ttl: u64,
+    },
+    TTL {
+        key: String,
+    },
     Keys,
     Count,
     Clear,
     Exit,
-    Unknown (String),
+    Unknown(String),
 }
 
 impl Command {
     pub fn parse(input: &str) -> Self {
-        let parts: Vec<&str> = input.trim().splitn(3, ' ').collect();
+        let parts: Vec<&str> = input.trim().splitn(4, ' ').collect();
 
         match parts.as_slice() {
-            ["GET", key] => Command::Get { key: key.to_string() },
-            ["SET", key, value] => Command::Set {
+            ["GET", key] => Command::Get {
                 key: key.to_string(),
-                value: value.to_string(),
             },
-            ["DELETE", key] => Command::Delete { key: key.to_string() },
+            ["SET", key, value, ttl] => {
+                let ttl = ttl.parse::<u64>();
+                match ttl {
+                    Ok(ttl_from_input) => Command::Set {
+                        key: key.to_string(),
+                        value: value.to_string(),
+                        ttl: ttl_from_input,
+                    },
+                    Err(_) => Command::Unknown(format!("Invalid TTL: {:?}", ttl)),
+                }
+            }
+            ["DELETE", key] => Command::Delete {
+                key: key.to_string(),
+            },
             ["RENAME", old_key, new_key] => Command::Rename {
                 old_key: old_key.to_string(),
                 new_key: new_key.to_string(),
+            },
+            ["EXPIRE", key, ttl] => {
+                let ttl = ttl.parse::<u64>();
+                match ttl {
+                    Ok(ttl_from_input) => Command::Expire {
+                        key: key.to_string(),
+                        ttl: ttl_from_input,
+                    },
+                    Err(_) => Command::Unknown(format!("Invalid TTL: {:?}", ttl)),
+                }
+            }
+            ["TTL", key] => Command::TTL {
+                key: key.to_string(),
             },
             ["KEYS"] => Command::Keys,
             ["COUNT"] => Command::Count,
             ["CLEAR"] => Command::Clear,
             ["EXIT"] => Command::Exit,
-            _ => Command::Unknown(input.to_string()),
+            _ => Command::Unknown(format!("Unknown command {}", input.to_string())),
         }
     }
 }
